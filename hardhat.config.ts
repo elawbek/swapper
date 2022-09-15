f98e7c8be430249cd17b334e2b6264129b755cea
ElawBek <kdmitrii86@gmail.com>
2022-09-15 21:22:51 +0300

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,64 @@
+import dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url:
+          process.env.ETHEREUM_MAINNET_URL !== undefined
+            ? process.env.ETHEREUM_MAINNET_URL
+            : "",
+      },
+    },
+    ethereum: {
+      url:
+        process.env.ETHEREUM_MAINNET_URL !== undefined
+          ? process.env.ETHEREUM_MAINNET_URL
+          : "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined
+          ? [process.env.PRIVATE_KEY]
+          : [""],
+    },
+
+    goerli_testnet: {
+      url:
+        process.env.GOERLI_TESTNET_URL !== undefined
+          ? process.env.GOERLI_TESTNET_URL
+          : "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined
+          ? [process.env.PRIVATE_KEY]
+          : [""],
+    },
+  },
+
+  gasReporter: {
+    enabled: true,
+    currency: "",
+  },
+
+  etherscan: {
+    apiKey: process.env.SCAN_API_KEY,
+  },
+};
+
+export default config;
